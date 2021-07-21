83e8f98d3c223b7bba155ed0f7c1194748f8cd5d
danielpdwalker <danielpdwalker@gmail.com>
2021-07-21 14:10:43 +0100

    Updated matatika_dataset_to_html.py to require rawdata, either from an env var (for azure pipeline use), or within the dataset yaml itself. Updated save location for the output html files, now in datasets/datasouce, where datasource is another env var

--- matatika_dataset_to_html.py
+++ matatika_dataset_to_html.py
@@ -1,6 +1,9 @@
 import bios
 import json
 import os
+import sys
+
+from pathlib import Path
 
 from matatika.dataset import Dataset
 from matatika import chartjs
@@ -11,9 +14,27 @@ plotter = ChartJSPlotter()
 for file in os.listdir("."):
     if file.endswith(".yaml") or file.endswith('.yml'):
         yaml_dict = bios.read(file)
-
+        
         new_dataset = Dataset.from_dict(yaml_dict)
 
-        my_dataset = chartjs.to_chart(new_dataset, json.loads(new_dataset.raw_data))
+        #Try to get env var of raw data
+        try:
+            yaml_file_name = Path(file).stem.upper()
+        
+            my_dataset = chartjs.to_chart(new_dataset, json.loads(os.getenv(yaml_file_name)))
+        
+        except:
+            #Try to get raw data from the dataset yml
+            try:
+                my_dataset = chartjs.to_chart(new_dataset, json.loads(new_dataset.raw_data))
+            except:
+                print('Error getting raw data from either the enviroment variable or the dataset yaml file itself.')
+                sys.exit(1)
+        
+        output_dir = os.getenv("datasource") or "pipeline"
 
-        plotter.save(**my_dataset, filename=yaml_dict['title'], keep_html=True)
+        os.mkdir('datasets')
+
+        os.mkdir('datasets/' + output_dir)
+
+        plotter.save(**my_dataset, filename='datasets/' + output_dir + '/' + yaml_dict['title'], keep_html=True)
